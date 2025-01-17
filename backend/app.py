4e88a1d7e3c281cd6db1f967efd39c34e59735c4
Eronixy <erroniz21@gmail.com>
2025-01-17 09:09:57 +0800

    Updated app.py

--- backend/app.py
+++ backend/app.py
@@ -5,16 +5,17 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-frontend_folder = os.path.join(os.getcwd(),",,", "frontend")
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
 dist_folder = os.path.join(frontend_folder, "dist")
 
-@app.route("/",defaults={"filename":""})
+@app.route("/", defaults={"filename": ""})
 @app.route("/<path:filename>")
 def index(filename):
-  if not filename:
-    filename = "index.html"
-  return send_from_directory(dist_folder,filename)
-
+    print(filename)
+    print(dist_folder)
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
 
 KEYWORDS = {
     word: category.upper().replace(' ', '_')
