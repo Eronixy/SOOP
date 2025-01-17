922f4b6b95471224a073715d125acaeb7f0e5eeb
Eronixy <erroniz21@gmail.com>
2025-01-17 08:37:27 +0800

    updated requirements.txt

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
+
+frontend_folder = os.path.join(os.getcwd(),",,", "frontend")
+dist_folder = os.path.join(frontend_folder, "dist")
+
+@app.route("/",defaults={"filename":""})
+@app.route("/<path:filename>")
+def index(filename):
+  if not filename:
+    filename = "index.html"
+  return send_from_directory(dist_folder,filename)
+
 
 KEYWORDS = {
     word: category.upper().replace(' ', '_')
